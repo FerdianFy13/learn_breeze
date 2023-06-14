db31563d7dfcb77376041d4d48d934d5a0b74c48
FerdianFy13 <ferdianfy13@gmail.com>
2023-06-14 10:47:26 +0700

    |FIX| update query edit category

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\CategoryProduct;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
 
 class CategoryController extends Controller
@@ -69,17 +70,40 @@ class CategoryController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit($id)
     {
-        //
+        $category = CategoryProduct::findOrFail($id);
+
+        return view('pages.category.update', [
+            'title' => 'Update Category',
+            'data' => $category,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $validation = $request->validate([
+                'name' => [
+                    'sometimes', 'required', Rule::unique('category_products', 'name')->ignore($id), 'max:255', 'min:3',
+                ],
+                'description' => 'sometimes|required|max:255|min:3',
+            ]);
+
+            $category = CategoryProduct::findOrFail($id);
+            $category->update($validation);
+
+            if ($category) {
+                return response()->json(['success' => 'Category updated successfully'], 201);
+            } else {
+                return response()->json(['error' => 'Failed to updated category'], 500);
+            }
+        } catch (ValidationException $e) {
+            return response()->json(['errors' => $e->errors()], 422);
+        }
     }
 
     /**
