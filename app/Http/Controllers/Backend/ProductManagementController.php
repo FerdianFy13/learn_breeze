951c8f60495f493164f77d17781850b9f4d012fd
FerdianFy13 <ferdianfy13@gmail.com>
2023-06-27 09:30:40 +0700

    |FIX| insert product management  and perf code

--- app/Http/Controllers/Backend/ProductManagementController.php
+++ app/Http/Controllers/Backend/ProductManagementController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProductUMKM;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class ProductManagementController extends Controller
 {
@@ -12,7 +15,10 @@ class ProductManagementController extends Controller
      */
     public function index()
     {
-        //
+        return view('pages.product_management.index', [
+            'title' => 'Product Management',
+            'data' => ProductUMKM::with('user')->orderBy('title', 'asc')->get()
+        ]);
     }
 
     /**
@@ -20,7 +26,10 @@ class ProductManagementController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.product_management.insert', [
+            'title' => 'Insert Product Management',
+            'data' => User::all(),
+        ]);
     }
 
     /**
@@ -28,7 +37,46 @@ class ProductManagementController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validation = $request->validate([
+                'user_id' => 'required|exists:users,id',
+                'title' => 'required|unique:product_u_m_k_m_s|max:255|min:3',
+                'description' => 'required|max:255|min:3',
+                'price' => [
+                    'required',
+                    'min:3',
+                    'max:10',
+                    'not_in:0',
+                    'regex:/^([1-9][0-9]*)$/'
+                ],
+                'phone_number' => [
+                    'required',
+                    'min:11',
+                    'max:13',
+                    'not_in:0',
+                    'regex:/^([1-9][0-9]*)$/'
+                ],
+                'instagram' => 'required|url|regex:/^(https?:\/\/)?(www\.)?instagram\.com\/[a-zA-Z0-9_]+\/?$/',
+                'available' => 'required',
+                'image' => 'required|image|file|max:4020',
+            ]);
+
+            if ($request->file('image')) {
+                $validation['image'] = $request
+                    ->file('image')
+                    ->store('product_management_images');
+            }
+
+            $partner = ProductUMKM::create($validation);
+
+            if ($partner) {
+                return response()->json(['success' => 'Product Management created successfully'], 201);
+            } else {
+                return response()->json(['error' => 'Failed to create product management'], 500);
+            }
+        } catch (ValidationException $e) {
+            return response()->json(['errors' => $e->errors()], 422);
+        }
     }
 
     /**
---
+++ resources/views/pages/product_management/index.blade.php
@@ -0,0 +1,152 @@
+@extends('layouts.backend')
+
+@section('backend_content')
+    <style>
+        .action-links {
+            display: flex;
+            flex-direction: row;
+        }
+
+        .action-links a {
+            margin-right: 10px;
+        }
+    </style>
+
+    <div class="row">
+        <div class="col-lg-12 d-flex align-items-strech">
+            <div class="card w-100">
+                <div class="card-body">
+                    <div class="d-sm-flex d-block align-items-center justify-content-between mb-9">
+                        <div class="mb-3 mb-sm-0">
+                            <h5 class="card-title fw-semibold">Table {{ $title }}</h5>
+                        </div>
+                    </div>
+                    <div class="table-responsive">
+                        <a href="/products/create" class="text-decoration-none btn btn-outline-dark mb-3"><i
+                                class="ti ti-plus me-1"></i>Add {{ $title }}</a>
+
+                        <table id="category" class="table" style="width:100%">
+                            <thead class="table-light">
+                                <tr>
+                                    <th>Actions</th>
+                                    <th>Title</th>
+                                    <th>Username</th>
+                                    <th>Description</th>
+                                    <th>Price</th>
+                                    <th>Phone Number</th>
+                                    <th>Instagram</th>
+                                    <th>Available</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($data as $item)
+                                    <tr>
+                                        <td class="action-links">
+                                            <a href="{{ route('products.edit', $item->id) }}"
+                                                class="text-decoration-none btn btn-outline-dark mb-3">Edit</a>
+                                            <div class="dropdown">
+                                                <button class="btn btn-outline-dark dropdown-toggle" type="button"
+                                                    id="moreActionsDropdown" data-bs-toggle="dropdown"
+                                                    aria-expanded="false">
+                                                    More
+                                                </button>
+                                                <ul class="dropdown-menu" aria-labelledby="moreActionsDropdown">
+                                                    <li><a class="dropdown-item"
+                                                            href="{{ route('products.show', $item->id) }}"><i
+                                                                class="ti ti-info-circle me-1 text-black"></i>Detail</a>
+                                                    </li>
+                                                    <form id="formDelete" method="post"
+                                                        action="{{ route('products.destroy', $item->id) }}">
+                                                        @method('delete')
+                                                        @csrf
+                                                        <input type="hidden" name="id" value="{{ $item->id }}">
+                                                        <li><button class="dropdown-item deleteButton"><i
+                                                                    class="ti ti-trash text-black me-1"
+                                                                    onclick="confirm?"></i>Delete</button>
+                                                        </li>
+                                                    </form>
+                                                </ul>
+                                            </div>
+                                        </td>
+                                        <td>{!! Str::limit($item->user->name, 20) !!}</td>
+                                        <td>{{ Str::limit($item->title, 20) }}</td>
+                                        <td>{!! Str::limit($item->description, 20) !!}</td>
+                                        <td>Rp{{ number_format($item->price, 2, ',', '.') }}</td>
+                                        <td>{!! Str::limit($item->phone_number, 20) !!}</td>
+                                        <td>{!! Str::limit($item->instagram, 20) !!}</td>
+                                        <td>{!! Str::limit($item->available, 20) !!}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            $('.deleteButton').click(function(e) {
+                e.preventDefault();
+
+                var itemId = $(this).closest('form').find('input[name="id"]').val();
+                var form = $(this).closest('form');
+
+                Swal.fire({
+                    icon: 'question',
+                    title: 'Are you sure?',
+                    text: "you want to delete this item!",
+                    showCancelButton: true,
+                    confirmButtonText: 'Yes, delete!',
+                    cancelButtonText: 'Cancel',
+                    confirmButtonColor: '#0F345E',
+                    cancelButtonColor: '#BB1F26',
+                    reverseButtons: true
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        $.ajax({
+                            url: form.attr('action'),
+                            type: 'POST',
+                            data: {
+                                _token: $('meta[name="csrf-token"]').attr('content'),
+                                _method: 'DELETE',
+                                item_id: itemId
+                            },
+                            success: function(response) {
+                                Swal.fire({
+                                    title: 'Success',
+                                    text: 'Delete data successfully',
+                                    icon: 'success',
+                                    confirmButtonColor: '#0F345E',
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        location.reload();
+                                    }
+                                });
+                            },
+                            error: function(xhr, status, error) {
+                                if (xhr.status === 422) {
+                                    Swal.fire({
+                                        title: 'Error',
+                                        text: 'Cannot delete item. It is still used in other records.',
+                                        icon: 'error',
+                                        confirmButtonColor: '#0F345E',
+                                    });
+                                } else {
+                                    Swal.fire({
+                                        title: 'Error',
+                                        text: 'Delete data failed',
+                                        icon: 'error',
+                                        confirmButtonColor: '#0F345E',
+                                    });
+                                }
+                            }
+
+                        });
+                    }
+                });
+            });
+        });
+    </script>
+@endsection
